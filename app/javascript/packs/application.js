1281b49225e040ac7b5e334fda39c55ddea8c69d
Andrekick <andrevskikh@gmail.com>
2020-12-10 16:06:48 +0300

    Fixed a bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,15 +7,14 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-
-import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/css/bootstrap'
-require("stylesheets/application.scss")
-import "@fortawesome/fontawesome-free/css/all"
-
+import "bootstrap/dist/css/bootstrap"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
+import "trix/dist/trix.css"
