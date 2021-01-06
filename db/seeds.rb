e76079cef681a194ecb4d6bc99b944a7f19c642f
Andrekick <andrevskikh@gmail.com>
2021-01-06 16:52:36 +0300

    added user_id

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,9 @@
+User.create!(email: 'admin@lol.com', password: 'adminko', password_confirmation: 'adminko')
+
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   }])
 end
